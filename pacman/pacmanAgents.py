f4d13068c7555e8ec27d3fc5296959d8dcaa3a0c
Changxing Cao <cc5766@nyu.edu>
2018-10-02 16:45:11 -0400

    first project, pacman game DFS + BFS + A star

---
+++ pacman/pacmanAgents.py
@@ -0,0 +1,106 @@
+# pacmanAgents.py
+# ---------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+from pacman import Directions
+from game import Agent
+from heuristics import *
+import random
+import collections as cls
+
+class RandomAgent(Agent):
+    # Initialization Function: Called one time when the game starts
+    def registerInitialState(self, state):
+        return;
+
+    # GetAction Function: Called with every frame
+    def getAction(self, state):
+        # get all legal actions for pacman
+        actions = state.getLegalPacmanActions()
+        # returns random action from all the valide actions
+        return actions[random.randint(0,len(actions)-1)]
+
+class OneStepLookAheadAgent(Agent):
+    # Initialization Function: Called one time when the game starts
+    def registerInitialState(self, state):
+        return;
+
+    # GetAction Function: Called with every frame
+    def getAction(self, state):
+        # get all legal actions for pacman
+        legal = state.getLegalPacmanActions()
+        # get all the successor state for these actions
+        successors = [(state.generatePacmanSuccessor(action), action) for action in legal]
+        # evaluate the successor states using scoreEvaluation heuristic
+        scored = [(admissibleHeuristic(state), action) for state, action in successors]
+        # get best choice
+        bestScore = min(scored)[0]
+        # get all actions that lead to the highest score
+        bestActions = [pair[1] for pair in scored if pair[0] == bestScore]
+        # return random action from the list of the best actions
+        return random.choice(bestActions)
+
+class BFSAgent(Agent):
+    # Initialization Function: Called one time when the game starts
+    def registerInitialState(self, state):
+        global scored
+        scored = []
+        return;
+
+    # GetAction Function: Called with every frame
+    def getAction(self, state):
+        # TODO: write BFS Algorithm instead of returning Directions.STOP
+        q = cls.deque()
+        q.append((state, Directions.STOP))
+        while q:
+            cur, action = q.pop()
+            if (cur.isWin()):
+                return action
+            if (cur.isLose()):
+                continue
+            legal = cur.getLegalPacmanActions()
+            # return action, state
+            successors = [(cur.generatePacmanSuccessor(action), action) for action in legal]
+            # global scored
+            # scored = scored + [(admissibleHeuristic(state), action) for state, action in successors]
+            # scored.add(scoredTemp)
+            for state, action in successors:
+                if (state != None):
+                    q.append((state, action))
+        # global scored
+        scored = [(admissibleHeuristic(state), action) for state, action in q]
+        if (scored == None):
+            return Directions.STOP
+        bestScore = min(scored)[0]
+        bestActions = [pair[1] for pair in scored if pair[0] == bestScore]
+        return random.choice(bestActions)
+
+class DFSAgent(Agent):
+    # Initialization Function: Called one time when the game starts
+    def registerInitialState(self, state):
+        return;
+
+    # GetAction Function: Called with every frame
+    def getAction(self, state):
+        # TODO: write DFS Algorithm instead of returning Directions.STOP
+        return Directions.STOP
+
+class AStarAgent(Agent):
+    # Initialization Function: Called one time when the game starts
+    def registerInitialState(self, state):
+        return;
+
+    # GetAction Function: Called with every frame
+    def getAction(self, state):
+        # TODO: write A* Algorithm instead of returning Directions.STOP
+        return Directions.STOP
