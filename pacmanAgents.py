b4302a9aa1c0068c3921fb758ebcc9021b3545f6
Changxing Cao <cc5766@nyu.edu>
2018-11-15 16:28:09 -0500

    ClimbHill Algorithm & MCTS Algorithm

--- pacmanAgents.py
+++ pacmanAgents.py
@@ -56,12 +56,55 @@ class RandomSequenceAgent(Agent):
 class HillClimberAgent(Agent):
     # Initialization Function: Called one time when the game starts
     def registerInitialState(self, state):
+        # nothing Initialization
         return;
 
     # GetAction Function: Called with every frame
     def getAction(self, state):
         # TODO: write Hill Climber Algorithm instead of returning Directions.STOP
-        return Directions.STOP
+        possible = state.getAllPossibleActions()
+        flag = False
+        # create action sequence
+        actSeq = list()
+        finScore = [gameEvaluation(state, state), [Directions.STOP]]
+        for i in range(5):
+            actSeq.append(random.choice(possible))
+
+        while True:
+            actionNext = list()
+            for i in range(5):
+                if (random.randint(0, 1) == 0):
+                    actionNext.append(actSeq[i])
+                else:
+                    actionNext.append(random.choice(possible))
+            stateCur = state
+            # perform all the sequence
+            for action in actionNext:
+                stateNext = stateCur.generatePacmanSuccessor(action)
+                if stateNext is not None:
+                    if not stateNext.isWin() and not stateNext.isLose():
+                        stateCur = stateNext
+                    # if win, just return.. (if comment this, it also works)
+                    if stateNext.isWin():
+                        return actionNext[0]
+                    if stateNext.isLose():
+                        break
+                    continue
+                else:
+                    flag = True
+                    break
+            if flag == True:
+                return finScore[1][0]
+            score = gameEvaluation(state, stateCur)
+            if (score > finScore[0]):
+                finScore[0] = score
+                finScore[1] = actionNext
+                # finScore[1].append(actionNext[0])
+                actSeq = actionNext
+            else:
+                continue
+
+        # return Directions.STOP
 
 class GeneticAgent(Agent):
     # Initialization Function: Called one time when the game starts
@@ -71,14 +114,109 @@ class GeneticAgent(Agent):
     # GetAction Function: Called with every frame
     def getAction(self, state):
         # TODO: write Genetic Algorithm instead of returning Directions.STOP
+        # DataStructure:
+        #  0                1       2
+        # [actionsequence, score, rank]
+
+        population = list()
+        possible = state.getAllPossibleActions()
+        for i in range(8):
+
+
+
+
+
         return Directions.STOP
 
 class MCTSAgent(Agent):
     # Initialization Function: Called one time when the game starts
     def registerInitialState(self, state):
+        #       0       1       2       3       4
+        # NODE (action, child, score, parent, visited)
         return;
 
     # GetAction Function: Called with every frame
     def getAction(self, state):
+        # Data Structure (instead)
+            # index  0       1       2       3       4
+            # NODE (action, child, score, parent, visited)
         # TODO: write MCTS Algorithm instead of returning Directions.STOP
-        return Directions.STOP
+
+        # count the UCT score to get best score child
+        def UCT(node, visitTimes):
+            # socre / visited
+            return node[2] / float(node[4]) + (math.sqrt(2 * math.log(float(visitTimes)) / float(node[4])))
+
+        def fullExpand(node, state):
+            actions = state.getLegalPacmanActions()
+            if (len(actions) == len(node[1])):
+                return True
+            return False
+
+        def treePolicy(node, state):
+            # Check if the current node is the leaf node
+            while True:
+                if fullExpand(node, state):
+                    # get best score child
+                    node = max(node[1], key = lambda x: UCT(x, node[4]))
+                else:
+                    return expand(node, state)
+            return node
+
+        def expand(node, state):
+            childAction = [child[0] for child in node[1]]
+            actions = state.getLegalPacmanActions()
+            for action in actions:
+                stateNext = state.generatePacmanSuccessor(action)
+                if stateNext is None:
+                    return None
+                if stateNext.isLose():
+                    return None
+                if action not in childAction:
+                    newNode = [action, [], 0.0, node, 1]
+                    node[1].append(newNode)
+                    return newNode
+            return None
+
+        def defaultPolicy(node, state):
+            # roll out 5 times
+            curState = state
+            actions = state.getLegalPacmanActions()
+            if len(actions) == 0:
+                return None
+            for i in range(5):
+                randomAction = random.choice(actions)
+                curState = curState.generatePacmanSuccessor(randomAction)
+                if curState == None:
+                    return None
+                if curState.isLose():
+                    return None
+            reward = gameEvaluation(rootState, curState)
+            return reward
+
+        def bp(node, reward):
+            while node != None:
+                node[4] = node[4] + 1
+                node[2] = node[2] + reward
+                node = node[3]
+            return
+
+        # Main Funtion:
+        rootState = state
+        root = [None, [], 0.0, None, 1]
+        while True:
+            expandNode = treePolicy(root, state)
+            if expandNode == None:
+                break
+            reward = defaultPolicy(expandNode, state)
+            if reward == None:
+                break
+            bp(expandNode, reward)
+
+        if len(root[1]) == 0:
+            return Directions.STOP
+        # get most visited node
+        bestVisit = max(root[1], key=lambda x: x[4])[4]
+        bestAction = [child[0] for child in root[1] if child[4] == bestVisit]
+
+        return random.choice(bestAction)
